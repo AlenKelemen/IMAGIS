3530debd11b3f11ddec9c9962a1407c0ffabe741
Alen Kelemen <alen@edc.hr>
2020-11-11 10:02:43 +0100

    remove ux & add code to index.js

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
-import UX from './src/UX';
-const ux = new UX();
+import 'ol/ol.css';
+import '@fortawesome/fontawesome-pro/css/fontawesome.css';
+import '@fortawesome/fontawesome-pro/css/regular.min.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import ScaleLine from 'ol/control/ScaleLine';
+import Rotate from 'ol/control/Rotate';
+import Zoom from 'ol/control/Zoom';
+import Projection from 'ol/proj/Projection';
+import {
+    register
+} from 'ol/proj/proj4.js';
+import proj4 from 'proj4';
+
+import def from './def.json';
+import Container from './src/container';
+
+proj4.defs('EPSG:3765',
+    '+proj=tmerc +lat_0=0 +lon_0=16.5 +k=0.9999 +x_0=500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
+register(proj4);
+const mapContainer = document.createElement('div');
+mapContainer.className = 'map';
+document.body.appendChild(mapContainer)
+const view = new View({
+    center: def.center,
+    zoom: def.zoom,
+    projection: new Projection({ //hr projection
+        code: 'EPSG:3765',
+        units: 'm',
+        extent: [208311.05, 4614890.75, 724721.78, 5159767.36]
+    })
+});
+window.map = new Map({
+    target: mapContainer,
+    view: view,
+    controls: [
+        new ScaleLine(),
+        new Rotate({
+            tipLabel: 'Sjever gore'
+        }),
+        new Zoom({
+            zoomInTipLabel: 'Približi',
+            zoomOutTipLabel: 'Udalji'
+        })
+    ]
+});
+const menubar = new Container({
+    className: 'menubar'
+});
+map.addControl(menubar);
+const sidebar = new Container({
+    className: 'sidebar'
+});
+map.addControl(sidebar);
+const actionbar = new Container({
+    className: 'actionbar'
+});
+map.addControl(actionbar);
+const statusbar = new Container({
+    className: 'statusbar'
+});
+map.addControl(statusbar);
