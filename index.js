b360cb8e37dfd0e501cec4fdebfff7ce03441600
Alen Kelemen <alen@edc.hr>
2020-11-13 14:00:13 +0100

    JSON.parse

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ import DefLayers from './src/defLayers';
 
 const path = window.location.href.split('/').slice(0, -1).join('/'); //base url
 if(!localStorage.getItem('def')) localStorage.setItem('def',JSON.stringify(def));
+const def = JSON.parse(localStorage.getItem('def'));
 proj4.defs('EPSG:3765',
     '+proj=tmerc +lat_0=0 +lon_0=16.5 +k=0.9999 +x_0=500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
 register(proj4);
