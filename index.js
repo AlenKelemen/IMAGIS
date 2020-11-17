3fb8448a350c6b171cd445ba0d3926915695b486
Alen Kelemen <alen@edc.hr>
2020-11-17 22:52:31 +0100

    epsg3765 as module

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ import View from 'ol/View';
 import ScaleLine from 'ol/control/ScaleLine';
 import Rotate from 'ol/control/Rotate';
 import Zoom from 'ol/control/Zoom';
-import Projection from 'ol/proj/Projection';
-import {
-    register
-} from 'ol/proj/proj4.js';
-import proj4 from 'proj4';
 
+import epsg3765 from './src/EPSG3765';
 import baseDef from './def.json';
 import Container from './src/container';
 import DefLayers from './src/defLayers';
@@ -23,24 +19,17 @@ if (localStorage.getItem('def') === null) localStorage.setItem('def', JSON.strin
 const def = JSON.parse(localStorage.getItem('def'));
 def.path = window.location.href.split('/').slice(0, -1).join('/'); //base url
 
-proj4.defs('EPSG:3765',
-    '+proj=tmerc +lat_0=0 +lon_0=16.5 +k=0.9999 +x_0=500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
-register(proj4);
 const mapContainer = document.createElement('main');
 mapContainer.className = 'map';
 document.body.appendChild(mapContainer)
 const view = new View({
     center: def.center,
     zoom: def.zoom,
-    projection: new Projection({ //hr projection
-        code: 'EPSG:3765',
-        units: 'm',
-        extent: [208311.05, 4614890.75, 724721.78, 5159767.36]
-    })
+    projection: new epsg3765()
 });
 const header = new Container({ //menu
     semantic: 'header',
-    className:'ol-control'
+    className: 'ol-control'
 });
 const footer = new Container({ //status
     semantic: 'footer'
