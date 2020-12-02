1ed4aead47a088b14a0f0ad3f4b3804c228815fd
Alen Kelemen <alen@edc.hr>
2020-12-02 21:06:19 +0100

    ..

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
-import 'ol/ol.css';
-import '@fortawesome/fontawesome-pro/css/fontawesome.css';
-import '@fortawesome/fontawesome-pro/css/regular.min.css';
-import './src/main.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import ScaleLine from 'ol/control/ScaleLine';
-import Rotate from 'ol/control/Rotate';
-import Zoom from 'ol/control/Zoom';
+import "ol/ol.css";
+import "@fortawesome/fontawesome-pro/css/fontawesome.css";
+import "@fortawesome/fontawesome-pro/css/regular.min.css";
+import "./src/main.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import ScaleLine from "ol/control/ScaleLine";
+import Rotate from "ol/control/Rotate";
+import Zoom from "ol/control/Zoom";
 
-
-import epsg3765 from './src/EPSG3765';
-import baseDef from './def.json';
-import Container from './src/container';
-import DefLayers from './src/defLayers';
-import DefEditor from './src/defEditor';
-import Geolocator from './src/geoloc';
-import Select from './src/select';
-import Legend from './src/legend';
+import epsg3765 from "./src/EPSG3765";
+import baseDef from "./def.json";
+import Container from "./src/container";
+import DefLayers from "./src/defLayers";
+import DefEditor from "./src/defEditor";
+import Geolocator from "./src/geoloc";
+import Select from "./src/select";
+import Legend from "./src/legend";
 
 //local project def
-if (localStorage.getItem('def') === null) localStorage.setItem('def', JSON.stringify(baseDef));
-const def = JSON.parse(localStorage.getItem('def'));
+if (localStorage.getItem("def") === null)
+  localStorage.setItem("def", JSON.stringify(baseDef));
+const def = JSON.parse(localStorage.getItem("def"));
 // ol/map
-const mapContainer = document.createElement('main');
-mapContainer.className = 'map';
-document.body.appendChild(mapContainer)
+const mapContainer = document.createElement("main");
+mapContainer.className = "map";
+document.body.appendChild(mapContainer);
 window.map = new Map({
-    target: mapContainer,
-    view: new View({
-        center: def.center,
-        zoom: def.zoom,
-        projection: new epsg3765()
-    }),
-    controls: []
+  target: mapContainer,
+  view: new View({
+    center: def.center,
+    zoom: def.zoom,
+    projection: new epsg3765()
+  }),
+  controls: []
 });
 //UX
-const header = new Container({ //menu
-    semantic: 'header',
-    className: 'ol-control'
+const header = new Container({
+  //menu
+  semantic: "header",
+  className: "ol-control"
 });
-const footer = new Container({ //status
-    semantic: 'footer'
+const footer = new Container({
+  //status
+  semantic: "footer"
 });
-const aside = new Container({ // contaner for left & right side menus, taskpanes etc
-    semantic: 'aside'
+const aside = new Container({
+  // contaner for left & right side menus, taskpanes etc
+  semantic: "aside"
 });
-const navRight = new Container({ // side menu right
-    semantic: 'nav',
-    className: 'nav-right'
+const navRight = new Container({
+  // side menu right
+  semantic: "nav",
+  className: "nav-right"
 });
-const navLeft = new Container({ // side menu left
-    semantic: 'nav',
-    className: 'nav-left'
+const navLeft = new Container({
+  // side menu left
+  semantic: "nav",
+  className: "nav-left"
 });
 
 //map.addControl(header);
@@ -61,20 +66,23 @@ map.addControl(footer);
 aside.addControl(navLeft);
 aside.addControl(navRight);
 navRight.addControl(new Zoom({}));
-navRight.addControl(new Rotate({
-    tipLabel: 'Sjever gore'
-}));
+navRight.addControl(
+  new Rotate({
+    tipLabel: "Sjever gore"
+  })
+);
 const legend = new Legend({
-    className: 'legend',
-    buttonControlClassName: 'ol-control',
-    html: '<i class="far fa-layer-group"></i>',
-    tipLabel: 'Legenda'
+  className: "legend",
+  buttonControlClassName: "ol-control",
+  html: '<i class="far fa-layer-group"></i>',
+  tipLabel: "Legenda",
+  target: navLeft.element
 });
-navLeft.addControl(legend);
+aside.addControl(legend);
 //layers as defined in def.json
 const defLayers = new DefLayers({
-    def: def,
-    map: map
+  def: def,
+  map: map
 });
 defLayers.addTileLayers();
 defLayers.addVectorLayers();
@@ -84,27 +92,29 @@ defLayers.addTHLayers();
 })); */
 //geolocate
 const geolocator = new Geolocator({
-    map: map,
-    className: 'geolocator ol-control',
-    html: '<i class="far fa-map-marker-alt"></i>',
-    tipLabel: 'Pokaži moju lokaciju'
+  map: map,
+  className: "geolocator ol-control",
+  html: '<i class="far fa-map-marker-alt"></i>',
+  tipLabel: "Pokaži moju lokaciju"
 });
 navRight.addControl(geolocator);
 //select
 const select = new Select({
-    active: true,
-    className: 'select-info'
+  active: true,
+  className: "select-info"
 });
 map.addInteraction(select);
-select.addInfo(footer, { className: 'select-info' });
+select.addInfo(footer, { className: "select-info" });
 select.addUI(navRight, {
-    point: {
-        className: 'select-point',
-        html: '<i class="far fa-mouse-pointer"></i>',
-        title: 'Odaberi objekte'
-    }
+  point: {
+    className: "select-point",
+    html: '<i class="far fa-mouse-pointer"></i>',
+    title: "Odaberi objekte"
+  }
 });
 //scale line - last control, variying width
-footer.addControl(new ScaleLine({
+footer.addControl(
+  new ScaleLine({
     target: footer.element
-}));
+  })
+);
--- src/legend.js
+++ src/legend.js
@@ -1,50 +1,56 @@
-import Control from 'ol/control/Control';
-import VectorLayer from 'ol/layer/Vector';
-import Sortable from 'sortablejs';
-const images = require('../img/*.gif');
+import Control from "ol/control/Control";
+import VectorLayer from "ol/layer/Vector";
+import Sortable from "sortablejs";
+const images = require("../img/*.gif");
 /** Container for controls
  * @constructor
  * @extends {ol_control_Control}
  * @param {Object} options Control options.
- * @param {string[='legend']} options.className 
- * @param {string} options.buttonControlClassName 
+ * @param {string[='legend']} options.className
+ * @param {string} options.buttonControlClassName
  */
 export default class Legend extends Control {
-    constructor(options = {}) {
-        super({
-            element: document.createElement('div')
-        });
-        this.element.className = options.className; //
-        this.buttonControl = document.createElement('div');
-        this.buttonControl.className = options.buttonControlClassName; //
-        this.buttonControl.style.display = 'inline-block';
-        this.button = document.createElement('button');
-        this.button.innerHTML = options.html || ''; //
-        this.button.title = options.tipLabel; //
-        this.button.addEventListener('click', evt => {
-            this.setActive(!this.getActive());
-        });
-        this.content = document.createElement('div');
-        this.content.className = 'content';
-        this.buttonControl.appendChild(this.button);
-        this.element.appendChild(this.buttonControl);
-        this.element.appendChild(this.content);
-    }
-    legend_() {
-        //in legend one can change layer opacitiy, visibility, zIndex, active property
-        const list = document.createElement('div');
-        list.className = 'ol-control legend-items';
-        this.content.appendChild(list);
-        const ls = this.getMap().getLayers().getArray().sort((a, b) => (a.getZIndex() > b.getZIndex()) ? 1 : -1).reverse(); //sort layers by index
-        for (const [i, l] of ls.entries()) {
-            const item = document.createElement('div');
-            list.appendChild(item);
-            item.id = l.get('name') || 'sloj ' + i;
-            l.set('name', item.id);
-            item.innerHTML = `
+  constructor(options = {}) {
+    super({
+      element: document.createElement("div")
+    });
+    this.element.className = "legend";
+    this.content = document.createElement("div");
+    this.content.className = "content";
+    this.element.appendChild(this.content);
+    const control = document.createElement("div");
+    control.className = options.className; //
+    const button = document.createElement("button");
+    button.innerHTML = options.html; //
+    button.title = options.tipLabel; //
+    button.addEventListener("click", evt => {
+      this.setActive(!this.getActive());
+    });
+    control.appendChild(button);
+    options.target.appendChild(control); // target element
+  }
+
+  legend_() {
+    //in legend one can change layer opacitiy, visibility, zIndex, active property
+    const list = document.createElement("div");
+    list.className = "ol-control legend-items";
+    this.content.appendChild(list);
+    const ls = this.getMap()
+      .getLayers()
+      .getArray()
+      .sort((a, b) => (a.getZIndex() > b.getZIndex() ? 1 : -1))
+      .reverse(); //sort layers by index
+    for (const [i, l] of ls.entries()) {
+      const item = document.createElement("div");
+      list.appendChild(item);
+      item.id = l.get("name") || "sloj " + i;
+      l.set("name", item.id);
+      item.innerHTML = `
                 <div class="header">
-                    <span class="icon" title="Rasterska podloga"><img src="${images.lc_raster}"></span>
-                    <span class="label">${l.get('label') || item.id}</span>
+                    <span class="icon" title="Rasterska podloga"><img src="${
+                      images.lc_raster
+                    }"></span>
+                    <span class="label">${l.get("label") || item.id}</span>
                     <div class="thematic"></div>
                     <div class="sub">
                         <span class="detail" title="Detalji"><i class="far fa-plus fa-fw"></i></span>
@@ -59,102 +65,130 @@ export default class Legend extends Control {
                     </div> 
                 </div>
                 `;
-            // show thematic legend on click
-            item.querySelector('.icon').addEventListener('click', evt => {
-                const e = item.querySelector('.thematic');
-                e.style.display = e.style.display === 'none' ? 'block' : 'none';
-            });
-            //  resolution/visibility zoom depending
-            const min = l.getMinResolution() || 0;
-            const max = l.getMaxResolution() || Infinity;
-            this.getMap().getView().on('change:resolution', () => {
-                const res = this.getMap().getView().getResolution();
-                item.style.display = res >= min && res <= max ? 'block' : 'none';
-            });
-            map.getView().dispatchEvent('change:resolution');
-            // opacity ! changes layer opacity
-            item.querySelector('.opacity').value = l.getOpacity();
-            item.querySelector('.opacity').addEventListener('change', evt => {
-                l.setOpacity(Number(item.querySelector('.opacity').value));
-            });
-            //  info
-            item.querySelector('.info').innerHTML = l.get('info') || '';
-            //  expand/shrink content
-            item.querySelector('.header .detail').addEventListener('click', evt => {
-                const content = item.querySelector('.content');
-                content.style.display = content.style.display === 'none' ? 'block' : 'none';
-                evt.currentTarget.innerHTML = content.style.display === 'none' ? '<i class="far fa-plus fa-fw"></i>' : '<i class="far fa-minus fa-fw"></i>';
-            });
-            //  show/hide on click ! changes layer visibility
-            item.querySelector('.header .visible').innerHTML = l.getVisible() ? '<i class="far fa-eye fa-fw"></i>' : '<i class="far fa-eye-slash fa-fw"></i>';
-            item.querySelector('.header .visible').addEventListener('click', evt => {
-                l.setVisible(!l.getVisible());
-                evt.currentTarget.innerHTML = l.getVisible() ? '<i class="far fa-eye fa-fw"></i>' : '<i class="far fa-eye-slash fa-fw"></i>';
-            });
-            //  active/inactive (for single layer select & similar action) ! changes layer active property
-            const active = item.querySelector('.header .active');
-            if (l instanceof VectorLayer === false) active.style.visibility = 'hidden';
-            active.innerHTML = l.get('active') ? '<i class="far fa-check-square fa-fw"></i>' : '<i class="far fa-square fa-fw"></i>';
-            active.addEventListener('click', evt => {
-                const oldValue = l.get('active');
-                for (const l_ of ls) {
-                    l_.set('active', false);
-                }
-                for (const i of list.querySelectorAll('.header .active')) {
-                    i.innerHTML = '<i class="far fa-square fa-fw"></i>';
-                }
-                l.set('active', !oldValue);
-                active.innerHTML = l.get('active') ? '<i class="far fa-check-square fa-fw"></i>' : '<i class="far fa-square fa-fw"></i>';
-                this.getMap().getLayers().set('active', l.get('active') ? l : null);
-            });
-            //icons
-            this.getMap().getView().on('change:resolution', () => {
-                //if (item.style.display !== 'none')
-                //this.getIcons(l, item.querySelector('.icon'), item.querySelector('.thematic'));
-            });
-            l.on('change:opacity', (evt) => {
-                item.querySelector('.icon').style.opacity = evt.target.getOpacity();
-            });
-            if (l.get('def')) { // set layer properties to def if def exists
-                l.on('propertychange', evt => {
-                    l.get('def').zIndex = evt.target.getZIndex();
-                    l.get('def').visible = evt.target.getVisible() === undefined ? true : evt.target.getVisible();
-                    l.get('def').opacity = evt.target.getOpacity() === undefined ? 1 : evt.target.getOpacity();
-                    l.get('def').active = evt.target.get('active') === undefined ? false : evt.target.get('active');
-                    //localStorage.setItem('def', JSON.stringify(this.getMap().get('def')));
-                });
-            }
+      // show thematic legend on click
+      item.querySelector(".icon").addEventListener("click", evt => {
+        const e = item.querySelector(".thematic");
+        e.style.display = e.style.display === "none" ? "block" : "none";
+      });
+      //  resolution/visibility zoom depending
+      const min = l.getMinResolution() || 0;
+      const max = l.getMaxResolution() || Infinity;
+      this.getMap()
+        .getView()
+        .on("change:resolution", () => {
+          const res = this.getMap()
+            .getView()
+            .getResolution();
+          item.style.display = res >= min && res <= max ? "block" : "none";
+        });
+      map.getView().dispatchEvent("change:resolution");
+      // opacity ! changes layer opacity
+      item.querySelector(".opacity").value = l.getOpacity();
+      item.querySelector(".opacity").addEventListener("change", evt => {
+        l.setOpacity(Number(item.querySelector(".opacity").value));
+      });
+      //  info
+      item.querySelector(".info").innerHTML = l.get("info") || "";
+      //  expand/shrink content
+      item.querySelector(".header .detail").addEventListener("click", evt => {
+        const content = item.querySelector(".content");
+        content.style.display =
+          content.style.display === "none" ? "block" : "none";
+        evt.currentTarget.innerHTML =
+          content.style.display === "none"
+            ? '<i class="far fa-plus fa-fw"></i>'
+            : '<i class="far fa-minus fa-fw"></i>';
+      });
+      //  show/hide on click ! changes layer visibility
+      item.querySelector(".header .visible").innerHTML = l.getVisible()
+        ? '<i class="far fa-eye fa-fw"></i>'
+        : '<i class="far fa-eye-slash fa-fw"></i>';
+      item.querySelector(".header .visible").addEventListener("click", evt => {
+        l.setVisible(!l.getVisible());
+        evt.currentTarget.innerHTML = l.getVisible()
+          ? '<i class="far fa-eye fa-fw"></i>'
+          : '<i class="far fa-eye-slash fa-fw"></i>';
+      });
+      //  active/inactive (for single layer select & similar action) ! changes layer active property
+      const active = item.querySelector(".header .active");
+      if (l instanceof VectorLayer === false)
+        active.style.visibility = "hidden";
+      active.innerHTML = l.get("active")
+        ? '<i class="far fa-check-square fa-fw"></i>'
+        : '<i class="far fa-square fa-fw"></i>';
+      active.addEventListener("click", evt => {
+        const oldValue = l.get("active");
+        for (const l_ of ls) {
+          l_.set("active", false);
         }
-        //  z-order
-        Sortable.create(list, {
-            handle: '.sort',
-            onEnd: evt => {
-                for (let i = 0; i < list.children.length; i++) {
-                    const layer = ls.find(x => x.get('name') === list.children[i].id);
-                    layer.setZIndex(list.children.length - i);
-                }
-            }
+        for (const i of list.querySelectorAll(".header .active")) {
+          i.innerHTML = '<i class="far fa-square fa-fw"></i>';
+        }
+        l.set("active", !oldValue);
+        active.innerHTML = l.get("active")
+          ? '<i class="far fa-check-square fa-fw"></i>'
+          : '<i class="far fa-square fa-fw"></i>';
+        this.getMap()
+          .getLayers()
+          .set("active", l.get("active") ? l : null);
+      });
+      //icons
+      this.getMap()
+        .getView()
+        .on("change:resolution", () => {
+          //if (item.style.display !== 'none')
+          //this.getIcons(l, item.querySelector('.icon'), item.querySelector('.thematic'));
         });
-    }
-    setActive(b) {
-        this.content.style.display = b ? 'inline-block' : 'none';
-        if (this.content.innerHTML === '') this.legend_();
-        if (this.getActive() == b) return;
-        if (b) {
-            this.button.classList.add('active');
-            if (this.getParent()) this.getParent().deactivateControls(this); //see container.js for deactivateControls
-        } else this.button.classList.remove('active');
-        this.dispatchEvent({
-            type: 'change:active',
-            key: 'active',
-            oldValue: !b,
-            active: b
+      l.on("change:opacity", evt => {
+        item.querySelector(".icon").style.opacity = evt.target.getOpacity();
+      });
+      if (l.get("def")) {
+        // set layer properties to def if def exists
+        l.on("propertychange", evt => {
+          l.get("def").zIndex = evt.target.getZIndex();
+          l.get("def").visible =
+            evt.target.getVisible() === undefined
+              ? true
+              : evt.target.getVisible();
+          l.get("def").opacity =
+            evt.target.getOpacity() === undefined ? 1 : evt.target.getOpacity();
+          l.get("def").active =
+            evt.target.get("active") === undefined
+              ? false
+              : evt.target.get("active");
+          //localStorage.setItem('def', JSON.stringify(this.getMap().get('def')));
         });
+      }
     }
-    getActive() {
-        return this.button.classList.contains('active');
-    }
-    getParent() {
-        if (this.get('parent')) return this.get('parent');
-    }
+    //  z-order
+    Sortable.create(list, {
+      handle: ".sort",
+      onEnd: evt => {
+        for (let i = 0; i < list.children.length; i++) {
+          const layer = ls.find(x => x.get("name") === list.children[i].id);
+          layer.setZIndex(list.children.length - i);
+        }
+      }
+    });
+  }
+  setActive(b) {
+    if (this.content.innerHTML === "") this.legend_();
+    if (this.getActive() == b) return;
+    if (b) {
+      this.element.classList.add("active");
+      if (this.getParent()) this.getParent().deactivateControls(this); //see container.js for deactivateControls
+    } else this.element.classList.remove("active");
+    this.dispatchEvent({
+      type: "change:active",
+      key: "active",
+      oldValue: !b,
+      active: b
+    });
+  }
+  getActive() {
+    return this.element.classList.contains("active");
+  }
+  getParent() {
+    if (this.get("parent")) return this.get("parent");
+  }
 }
