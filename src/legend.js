beab2c054e2092ba40c0a19df5f1a676a7ba3e8e
Alen Kelemen <alen@edc.hr>
2021-01-15 21:53:00 +0100

    icon

--- src/legend.js
+++ src/legend.js
@@ -2,6 +2,7 @@ import "@fortawesome/fontawesome-pro/css/fontawesome.css";
 import "@fortawesome/fontawesome-pro/css/regular.min.css";
 import Container from "./container";
 import Toggle from "./toggle";
+import { toContext } from "ol/render";
 const images = require("../img/*.gif");
 
 /** thematic editor
@@ -23,6 +24,7 @@ export default class Legend extends Toggle {
     this.container = new Container({ semantic: "section", className: options.contanerClassName });
     options.target.addControl(this.container);
     this.className = options.class || "legend";
+    this.iconSize = options.iconSize || [14, 14];
     this.container.element.classList.add("hidden");
     this.on("change:active", (evt) => this.container.setVisible(evt.active));
     this.header = document.createElement("header");
@@ -34,8 +36,19 @@ export default class Legend extends Toggle {
     this.cfg.layers.sort((a, b) => (a.zIndex > b.zIndex ? 1 : -1)); //zIndex as loaded in map by def.js
     this.cfg.layers.map((x) => this.addItem(x));
   }
-
-  addItem(prop,iconSize = [15, 15]) {
+   /**
+   * 
+   * 
+   * @param {string} cfg.layer.name 
+   * @returns 
+   * 
+   * @memberOf Legend
+   */
+  source(name) {
+    const layer = this.cfg.layers.find(x => x.name === name);
+    return this.cfg.sources.find((x) => layer.source === x.name);
+  }
+  addItem(prop) {
     const item = Object.assign(document.createElement("section"), { className: `${this.className}-item` }),
       itemHeader = Object.assign(document.createElement("header"), { className: `${this.className}-item-header` }),
       itemArticle = Object.assign(document.createElement("article"), { className: `${this.className}-item-article` }),
@@ -47,12 +60,22 @@ export default class Legend extends Toggle {
     item.id = prop.name;
     // item header
     itemHeader.innerHTML = `
-      <span class="${this.className}-item-header-icon" title="Rasterska podloga">
-        <canvas width="${iconSize[0]}" height="${iconSize[1]}"></canvas>
-        </span>
-      <span class="${this.className}-item-header-label">${prop.label || prop.name}</span>`;
-   
- 
+      <span class="${this.className}-item-header-icon">
+        <canvas width="${this.iconSize[0]}" height="${this.iconSize[1]}"></canvas>
+      </span>
+      <span class="${this.className}-item-header-label">${prop.label || prop.name}</span>
+      <div class="${this.className}-item-header-thematic"></div>`;
+    const canvas = itemHeader.querySelector("canvas");
+    this.icon(canvas, this.source(prop.name).type);
   }
- 
+  icon(canvas, type) {
+    const ctx = canvas.getContext("2d"),
+      img = new Image();
+    img.src = images.lc_raster;
+    if (["geojson", "th"].indexOf(type) > -1) {
+      if()
+      img.src = images.lc_theme;
+    }
+    img.onload = () => ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+  }
 }
