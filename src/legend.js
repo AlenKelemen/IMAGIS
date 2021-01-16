f5e58c14558a9f0b4554689d42043f19013a21d7
Alen Kelemen <alen@edc.hr>
2021-01-16 22:11:19 +0100

    ...

--- src/legend.js
+++ src/legend.js
@@ -1,6 +1,6 @@
 import "@fortawesome/fontawesome-pro/css/fontawesome.css";
 import "@fortawesome/fontawesome-pro/css/regular.min.css";
-import {elt} from "./util";
+import { elt } from "./util";
 import Container from "./container";
 import Toggle from "./toggle";
 import { toContext } from "ol/render";
@@ -28,31 +28,32 @@ export default class Legend extends Toggle {
     this.iconSize = options.iconSize || [14, 14];
     this.container.element.classList.add("hidden");
     this.on("change:active", (evt) => this.container.setVisible(evt.active));
-    this.header = elt("header",{className:`${this.className}-header`},"Prostorni slojevi");
+    this.header = elt("header", { className: `${this.className}-header` }, "Prostorni slojevi");
     this.container.element.appendChild(this.header);
-    this.items = elt('article', {className: `${this.className}-items`});
+    this.items = elt("article", { className: `${this.className}-items` });
     this.container.element.appendChild(this.items);
-    this.footer = elt("footer",{className:`${this.className}-footer`},"Footer");
+    this.footer = elt("footer", { className: `${this.className}-footer` }, "Footer");
     this.container.element.appendChild(this.footer);
     this.cfg.layers.sort((a, b) => (a.zIndex > b.zIndex ? 1 : -1)); //zIndex as loaded in map by def.js
     this.cfg.layers.map((x) => this.addItem(x));
-    
   }
-   /**
-   * 
-   * 
-   * @param {string} cfg.layer.name 
-   * @returns 
-   * 
+  /**
+   *
+   *
+   * @param {string} cfg.layer.name
+   * @returns
+   *
    * @memberOf Legend
    */
- 
+
   addItem(prop) {
-    const thematic = elt('nav', {className: `${this.className}-items-item-header-thematic`});
-    const header = elt('header', {className: `${this.className}-items-item-header`},thematic);
-    const article = elt('article', {className: `${this.className}-items-item-article`});
-    const footer = elt('footer', {className: `${this.className}-items-item-footer`});
-    const item = elt('section', {className: `${this.className}-items-item`},header,article,footer);
+    const thematic = elt("nav", { className: `${this.className}-items-item-header-thematic` });
+    //const headerIcon = elt('canvas',{className:`${this.className}-item-header-icon`});
+    const header = elt("header", { className: `${this.className}-items-item-header` }, thematic,headerIcon);
+    const article = elt("article", { className: `${this.className}-items-item-article` });
+    const footer = elt("footer", { className: `${this.className}-items-item-footer` });
+    const item = elt("section", { className: `${this.className}-items-item`, id: prop.name }, header, article, footer);
     this.items.appendChild(item);
+    
   }
 }
